bb098eb3e178d34d29972ccb86429ed2a1f92ee5
aluvse <dev.aluvse@gmail.com>
2026-04-10 23:39:01 +0600

    feat: add solution #0009

---
+++ solutions/0009_palindrome_number/main.go
@@ -0,0 +1,12 @@
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	original, reversed := x, 0
+	for x > 0 {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+	return original == reversed
+}
